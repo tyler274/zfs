c0a0941079e230df0dab56d3912055c0b7fc48bb
Saso Kiselkov <saso.kiselkov@nexenta.com>
2015-04-20 15:32:08 +0200

    6363 Add UNMAP/TRIM functionality to ZFS
    Ported by: Tim Chase <tim@chase2k.com>
    
    Porting notes:
        The trim kstats are in zfs/<pool> along with the other per-pool stats.
        The kstats can be cleared by writing to the kstat file.
    
        Null format parameters to strftime() were replaced with "%c".
    
        Added vdev trace support.
    
        New dfl_alloc() function in the SPL is used to allocate arrays of
        dkioc_free_list_t objects since they may be large enough to require
        virtual memory.
    
    Other changes:
        Suppressed kstat creation for pools with "$" names.
    
        The changes to vdev_raidz_map_alloc() have been minimized in order to allow
        more conflict-free merging with future changes (ABD).
    
    Added the following module parameters:
        zfs_trim - Enable TRIM
        zfs_trim_min_ext_sz - Minimum size to trim
        zfs_txgs_per_trim - Transaction groups over which to batch trims

--- include/sys/metaslab_impl.h
+++ include/sys/metaslab_impl.h
@@ -25,6 +25,7 @@
 
 /*
  * Copyright (c) 2011, 2015 by Delphix. All rights reserved.
+ * Copyright 2016 Nexenta Systems, Inc. All rights reserved.
  */
 
 #ifndef _SYS_METASLAB_IMPL_H
@@ -158,6 +159,11 @@ struct metaslab_group {
 	uint64_t		mg_histogram[RANGE_TREE_HISTOGRAM_SIZE];
 };
 
+typedef struct {
+	uint64_t	ts_birth;	/* TXG at which this trimset starts */
+	range_tree_t	*ts_tree;	/* tree of extents in the trimset */
+} metaslab_trimset_t;
+
 /*
  * This value defines the number of elements in the ms_lbas array. The value
  * of 64 was chosen as it covers all power of 2 buckets up to UINT64_MAX.
@@ -226,10 +232,14 @@ struct metaslab {
 	uint64_t	ms_size;
 	uint64_t	ms_fragmentation;
 
-	range_tree_t	*ms_alloctree[TXG_SIZE];
-	range_tree_t	*ms_freetree[TXG_SIZE];
-	range_tree_t	*ms_defertree[TXG_DEFER_SIZE];
-	range_tree_t	*ms_tree;
+	range_tree_t		*ms_alloctree[TXG_SIZE];
+	range_tree_t		*ms_freetree[TXG_SIZE];
+	range_tree_t		*ms_defertree[TXG_DEFER_SIZE];
+	range_tree_t		*ms_tree;
+	metaslab_trimset_t	*ms_cur_ts; /* currently prepared trims */
+	metaslab_trimset_t	*ms_prev_ts;  /* previous (aging) trims */
+	kcondvar_t		ms_trim_cv;
+	metaslab_trimset_t	*ms_trimming_ts;
 
 	boolean_t	ms_condensing;	/* condensing? */
 	boolean_t	ms_condense_wanted;
