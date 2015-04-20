81c271d7590888542f9d5af433164f3b0548cbca
Saso Kiselkov <saso.kiselkov@nexenta.com>
2015-04-20 15:32:08 +0200

    6363 Add UNMAP/TRIM functionality to ZFS
    Ported by: Tim Chase <tim@chase2k.com>
    
    Porting notes:
        The trim kstats are in zfs/<pool> along with the other per-pool stats.
        The kstats can be cleared by writing to the kstat file.
    
        Null format parameters to strftime() were replaced with "%c".
    
        Added vdev trace support.
    
        New dfl_alloc() function in the SPL is used to allocate arrays of
        dkioc_free_list_t objects since they may be large enough to require
        virtual memory.
    
    Other changes:
        Suppressed kstat creation for pools with "$" names.
    
        The changes to vdev_raidz_map_alloc() have been minimized in order to allow
        more conflict-free merging with future changes (ABD).
    
    Added the following module parameters:
        zfs_trim - Enable TRIM
        zfs_trim_min_ext_sz - Minimum size to trim
        zfs_txgs_per_trim - Transaction groups over which to batch trims

--- lib/libzfs/libzfs_util.c
+++ lib/libzfs/libzfs_util.c
@@ -23,6 +23,7 @@
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.
+ * Copyright 2016 Nexenta Systems, Inc. All rights reserved.
  */
 
 /*
