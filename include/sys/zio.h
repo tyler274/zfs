427858ef8ba7aee0c2e9f638f7d90df68643721f
Saso Kiselkov <saso.kiselkov@nexenta.com>
2015-04-20 15:32:08 +0200

    6363 Add UNMAP/TRIM functionality to ZFS
    Ported by: Tim Chase <tim@chase2k.com>
    
    Porting notes:
        The trim kstats are in zfs/<pool> along with the other per-pool stats.
        The kstats can be cleared by writing to the kstat file.
    
        Null format parameters to strftime() were replaced with "%c".
    
        Added vdev trace support.
    
        New dfl_alloc() function in the SPL is used to allocate arrays of
        dkioc_free_list_t objects since they may be large enough to require
        virtual memory.
    
    Other changes:
        Suppressed kstat creation for pools with "$" names.
    
        The changes to vdev_raidz_map_alloc() have been minimized in order to allow
        more conflict-free merging with future changes (ABD).
    
    Added the following module parameters:
        zfs_trim - Enable TRIM
        zfs_trim_min_ext_sz - Minimum size to trim
        zfs_txgs_per_trim - Transaction groups over which to batch trims

--- include/sys/zio.h
+++ include/sys/zio.h
@@ -21,11 +21,11 @@
 
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright 2011 Nexenta Systems, Inc. All rights reserved.
  * Copyright (c) 2012, 2017 by Delphix. All rights reserved.
  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
  * Copyright 2016 Toomas Soome <tsoome@me.com>
+ * Copyright 2017 Nexenta Systems, Inc.  All rights reserved.
  */
 
 #ifndef _ZIO_H
@@ -240,6 +240,8 @@ typedef void zio_done_func_t(zio_t *zio);
 
 extern int zio_dva_throttle_enabled;
 extern const char *zio_type_name[ZIO_TYPES];
+
+struct range_tree;
 
 /*
  * A bookmark is a four-tuple <objset, object, level, blkid> that uniquely
@@ -500,6 +502,10 @@ extern zio_t *zio_claim(zio_t *pio, spa_t *spa, uint64_t txg,
 
 extern zio_t *zio_ioctl(zio_t *pio, spa_t *spa, vdev_t *vd, int cmd,
     zio_done_func_t *done, void *private, enum zio_flag flags);
+
+extern zio_t *zio_trim(zio_t *pio, spa_t *spa, vdev_t *vd,
+    struct range_tree *tree, zio_done_func_t *done, void *private,
+    enum zio_flag flags, int dkiocfree_flags, metaslab_t *msp);
 
 extern zio_t *zio_read_phys(zio_t *pio, vdev_t *vd, uint64_t offset,
     uint64_t size, struct abd *data, int checksum,
