bfa186999d2a2d8b2875bdb1c2a91be9ffeeef71
Tim Chase <tim@onlight.com>
2017-04-10 11:41:11 -0500

    Want manual trim feature to skip never-allocated space
    Some storage backends such as large thinly-provisioned SANs are very
    slow for large trims.  Manual trim now supports "zpool trim -p" to skip
    metaslabs for which there is no spacemap.

--- module/zfs/vdev.c
+++ module/zfs/vdev.c
@@ -3738,9 +3738,11 @@ vdev_deadman(vdev_t *vd)
  * Implements the per-vdev portion of manual TRIM. The function passes over
  * all metaslabs on this vdev and performs a metaslab_trim_all on them. It's
  * also responsible for rate-control if spa_man_trim_rate is non-zero.
+ *
+ * If fulltrim is set, metaslabs without spacemaps are also trimmed.
  */
-void
-vdev_man_trim(vdev_trim_info_t *vti)
+static void
+vdev_man_trim_impl(vdev_trim_info_t *vti, boolean_t fulltrim)
 {
 	clock_t t = ddi_get_lbolt();
 	spa_t *spa = vti->vti_vdev->vdev_spa;
@@ -3759,6 +3761,9 @@ vdev_man_trim(vdev_trim_info_t *vti)
 		uint64_t delta;
 		metaslab_t *msp = vd->vdev_ms[i];
 		zio_t *trim_io;
+
+		if (msp->ms_sm == NULL && !fulltrim)
+			continue;
 
 		trim_io = metaslab_trim_all(msp, &cursor, &delta, &was_loaded);
 		spa_config_exit(spa, SCL_STATE_ALL, FTAG);
@@ -3822,6 +3827,18 @@ out:
 	vti->vti_done_cb(vti->vti_done_arg);
 
 	kmem_free(vti, sizeof (*vti));
+}
+
+void
+vdev_man_trim(vdev_trim_info_t *vti)
+{
+	vdev_man_trim_impl(vti, B_FALSE);
+}
+
+void
+vdev_man_trim_full(vdev_trim_info_t *vti)
+{
+	vdev_man_trim_impl(vti, B_TRUE);
 }
 
 /*
